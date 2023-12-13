9eb3e3cd479b7c4e4a1ad8cd596e1d11801a333c
Jaime Escalona <jaescalo@akamai.com>
2023-12-13 09:09:09 -0600

    module compliant structure

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,16 @@
 output "contract_id" {
+    description = "Akamai Contract ID"
   value = data.akamai_contract.contract.id
 }
 
 output "group_id" {
+  description = "Aakamai Group ID"
   value = data.akamai_contract.contract.group_id
 }
 
 output "property_id" {
-  value = akamai_property.wingmanstrums-com.id
+  description = "Akamai Property ID"
+  value = akamai_property.my-resource.id
 }
 
 
